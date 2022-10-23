ae43143fede9a50acd43f2e0555cd1f23f610299
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-10-23 15:32:39 +0900

    add: job & trigger tutorial

--- src/main/kotlin/com/example/schedules/schedule/QuartzSchedule.kt
+++ src/main/kotlin/com/example/schedules/schedule/QuartzSchedule.kt
@@ -1,34 +1,68 @@
 package com.example.schedules.schedule
 
-import org.quartz.Job
-import org.quartz.JobExecutionContext
-import org.quartz.JobExecutionException
+import com.example.schedules.job.QuartzScheduleJob
+import com.example.schedules.trigger.QuartzTriggerBuilder
+import com.example.schedules.job.QuartzJobBuilder
+import org.quartz.JobBuilder.newJob
+import org.quartz.SimpleScheduleBuilder.simpleSchedule
+import org.quartz.TriggerBuilder.newTrigger
+import org.quartz.impl.StdSchedulerFactory
+import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
 
-class QuartzSchedule : Job {
+@Component
+class QuartzSchedule {
+
+
     /**
+     * **일정 요소**
+     *
+     * * schedule - 스케줄러와 상호작용하기 위한 API
      *
      *
-     * Called by the `[Scheduler]` when a `[Trigger]`
-     * fires that is associated with the `Job`.
+     * * Job - 스케줄러에 의해 실행되기 원하는 작업의 세부 정보를 정의
      *
      *
+     * * JobDetail - 작업을 정의하는데 사용
      *
      *
-     * The implementation may wish to set a
-     * [result][JobExecutionContext.setResult] object on the
-     * [JobExecutionContext] before this method exits.  The result itself
-     * is meaningless to Quartz, but may be informative to
-     * `[JobListener]s` or
-     * `[TriggerListener]s` that are watching the job's
-     * execution.
+     * * Trigger - 작업이 실행될 일정을 정의
      *
      *
-     * @throws JobExecutionException
-     * if there is an exception while executing the job.
+     * * JobBuilder - 작업을 정의하는 JobDetail을 정의하는데 사용
+     *
+     *
+     * * TriggerBuilder - 트리거를 정의하는데 사용
      */
-    override fun execute(context: JobExecutionContext?) {
+    @PostConstruct
+    fun setUpJob() {
+        val scheduler = StdSchedulerFactory().scheduler
+
+        scheduler.start()
+
+        /**
+         * JobDetail에 대한 설명 - [QuartzJobBuilder]
+         */
+        val job = newJob(QuartzScheduleJob::class.java)
+            .withIdentity("QUARTZ_JOB", "GROUP")
+            .build()
 
 
-        println("${System.currentTimeMillis()}")
+        /**
+         * Trigger에 대한 설명 - [QuartzTriggerBuilder]
+         */
+        val trigger = newTrigger()
+            .withIdentity("QUARTZ_TRIGGER", "GROUP")
+            .startNow()
+            .withSchedule(
+                simpleSchedule()
+                    .withIntervalInSeconds(10)
+                    .withRepeatCount(5)
+            ).build()
+
+
+        // scheduleJob - 트리거를 사용해 작업을 예약
+        scheduler.scheduleJob(job, trigger)
     }
+
 }
